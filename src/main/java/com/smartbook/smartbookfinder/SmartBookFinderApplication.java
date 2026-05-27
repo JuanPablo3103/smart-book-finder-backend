46d4243380b5d8a0b14f06f89b7532353638a55a
JuanPablo3103 <juanp.castrom@unac.edu.co>
2026-05-26 19:00:56 -0500

    Initial project structure

---
+++ src/main/java/com/smartbook/smartbookfinder/SmartBookFinderApplication.java
@@ -0,0 +1,13 @@
+package com.smartbook.smartbookfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBookFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBookFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartbook/smartbookfinder/SmartBookFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbook.smartbookfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBookFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
